c442e10dd097002e85eb32d2162a828e48be4e4c
Nilderson Chavez <nildersonalexis@gmail.com>
2022-08-12 10:16:06 -0600

    creacion de menu interno

--- principal.cs
+++ principal.cs
@@ -23,9 +23,72 @@ namespace semana_3_ejemplo_de_menu_en_consola
             System.Console.ReadKey();
 
             int repetir;
-            
+
             //menu_interno();
-            do 
+            do
+            {
+                Console.Clear();
+                 menu_interno();
+
+                System.Console.WriteLine("Ingrese 1 para repetir menú");
+                System.Console.WriteLine("Ingrese 2 para salir");
+                repetir = Int32.Parse(Console.ReadLine());
+
+            } while (repetir == 1);
+        }//Cierre de función principal;
+        //Inicio de funciones interna
+        static void menu_interno()
+        {
+            string opcion;
+            int opcionint;
+
+            System.Console.WriteLine("Menu Principal");
+            System.Console.WriteLine("1. Ejemplo If");
+            System.Console.WriteLine("2. Ejemplo Switch");
+            System.Console.WriteLine("3. Ejemplo For");
+            System.Console.WriteLine("4. Ejemplo Wor");
+            System.Console.WriteLine("5. Ejemplo Do While");
+
+            System.Console.WriteLine("Selecciona tu opcion: ");
+            opcion = Console.ReadLine();
+            opcionint = Int32.Parse(opcion);
+
+            switch (opcionint)
+            {
+                case 1:
+
+                    programa_if();
+
+                    break;
+
+                case 2:
+
+                    programa_switch();
+                    break;
+
+                case 3:
+
+                    programa_for();
+                    break;
+
+                case 4:
+
+                    programa_while();
+                    break;
+                case 5:
+
+                    programa_dowhile();
+                    break;
+
+                default:
+                    System.Console.WriteLine("opcion no valida");
+                    break;
+
+            }
+
+
+
         }
-    }
-}
+   
+
+
