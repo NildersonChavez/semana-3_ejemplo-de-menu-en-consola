9278a5e1fd3acddf89f261f6ccf17a31309c4d74
Nilderson Chavez <nildersonalexis@gmail.com>
2022-08-12 13:48:12 -0600

    ejemplo de clase dowhile

--- Ejemplo_dowhile.cs
+++ Ejemplo_dowhile.cs
@@ -8,5 +8,9 @@ namespace semana_3_ejemplo_de_menu_en_consola
 {
     class Ejemplo_dowhile
     {
+        public static void estructura_dowhile()
+        {
+            System.Console.WriteLine("dowhile externo");
+        }
     }
 }
