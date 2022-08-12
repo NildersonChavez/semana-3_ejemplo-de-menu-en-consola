51d1b7375005addd41f85cad6c5ebb5419f5fba3
Nilderson Chavez <nildersonalexis@gmail.com>
2022-08-12 13:50:46 -0600

    ejemplo clase While

--- Ejemplo_While.cs
+++ Ejemplo_While.cs
@@ -8,5 +8,9 @@ namespace semana_3_ejemplo_de_menu_en_consola
 {
     class Ejemplo_While
     {
+        public static void estructura_While()
+        {
+            System.Console.WriteLine("While externo");
+        }
     }
 }
