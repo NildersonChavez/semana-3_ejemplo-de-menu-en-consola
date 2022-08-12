e7d6671863a84b2a4f28818fb237d10aca2221d7
50364 <50364@DESKTOP-88RFI90>
2022-08-12 09:45:07 -0600

    Agregar archivos de proyecto.

---
+++ principal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static semana_3_ejemplo_de_menu_en_consola.Ejemplo_if;
+using static semana_3_ejemplo_de_menu_en_consola.Ejemplo_Switch;
+using static semana_3_ejemplo_de_menu_en_consola.Ejemplo_for;
+using static semana_3_ejemplo_de_menu_en_consola.Ejemplo_dowhile;
+using static semana_3_ejemplo_de_menu_en_consola. Ejemplo_While;
+
+namespace semana_3_ejemplo_de_menu_en_consola
+{
+    class principal
+    {
+        //Funcion principal que inicia el programa
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Proyecto de Menu");
+            System.Console.WriteLine("Primer Programa Usando Estructuras");
+
+            System.Console.ReadKey();
+
+            int repetir;
+
+            //menu_interno();
+        }
+    }
+}
